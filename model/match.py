3783618562fff2742c1263c41de85dc0a3dcff92
newkeros <albert.greg@gmail.com>
2021-04-18 11:37:36 +0200

    completion liste players et imports dans le controler

--- model/match.py
+++ model/match.py
@@ -1,9 +1,23 @@
 class Match:
-    def __init__(self, player1, player2):
+    def __init__(self, player1, player2, player3, player4, player5, player6, player7, player8):
         self.player1 = player1
         self.player2 = player2
+        self.player3 = player3
+        self.player4 = player4
+        self.player5 = player5
+        self.player6 = player6
+        self.player7 = player7
+        self.player8 = player8
         self.score_player1 = 0
         self.score_player2 = 0
+        self.score_player3 = 0
+        self.score_player4 = 0
+        self.score_player5 = 0
+        self.score_player6 = 0
+        self.score_player7 = 0
+        self.score_player8 = 0
+
+
 
 # Victoire 1 points 0 pour l'autre
 # Match nul 0.5 pour les deux
