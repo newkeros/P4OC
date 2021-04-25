922df554a2198fb29383431b58c790f2cdaff474
newkeros <albert.greg@gmail.com>
2021-04-25 20:54:02 +0200

    added running first round

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from controler.tournament_controler import TournamentControler
 
 tournamentControler = TournamentControler()
 tournamentControler.print_player()
+tournamentControler.run_first_round()
