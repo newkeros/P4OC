ca6868f7fc2f8e1a364ad9b3a12091ecfe4222a2
newkeros <albert.greg@gmail.com>
2021-04-27 21:08:57 +0200

    date checker ajouté

--- controler/tournament_controler.py
+++ controler/tournament_controler.py
@@ -1,7 +1,7 @@
 from model.tournament_model import Tournament
 from model.player import Player
 from model.round import Round
-from view.tournament_view import get_tournament_name, get_tournament_time_control
+from view.tournament_view import get_tournament_name, get_tournament_time_control, get_tournament_date
 from view.player_view import print_player, get_player_info
 from view.round_view import print_match_result, enter_score, print_final_round_score
 
@@ -13,7 +13,8 @@ class TournamentControler:
     def __init__(self):
         name = self.check_tournament_name()
         time_control = self.check_tournament_time_control()
-        self.tournament = Tournament(name, time_control)
+        tournament_date = get_tournament_date()
+        self.tournament = Tournament(name, time_control, tournament_date)
         # self.tournament = Tournament("test", "bullet") #BYPASS TOURNAMENT
 
         """for i in range(4):
@@ -40,6 +41,13 @@ class TournamentControler:
             time_control = get_tournament_time_control()
         return time_control
 
+    def check_tournament_date(self):
+        tournament_date = get_tournament_date()
+        while not tournament_date.isalpha():
+            print("Erreur de saisie: la date ne peut contenir que des chiffres")
+            tournament_date = get_tournament_date()
+        return tournament_date
+
 
     def run_first_round(self):
         # algorithme pour créer les premier round
@@ -53,7 +61,7 @@ class TournamentControler:
             match.score_player1, match.score_player2 = self.handle_score()
             print_match_result(match)
             self.update_player_score(match)
-            print_final_round_score(self.tournament.rounds[0].matchs, round1.number)
+        print_final_round_score(self.tournament.rounds[0].matchs, round1.number)
 
 
     def run_round(self, number):
