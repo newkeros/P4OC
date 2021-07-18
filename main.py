2ae643af54f743daf0698e918d380dc750745e88
newkeros <albert.greg@gmail.com>
2021-07-18 10:53:28 +0200

    ajout tinydb

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ from controler.tournament_controler import TournamentControler
 tournamentControler = TournamentControler()
 tournamentControler.print_player()
 tournamentControler.run_first_round()
+for i in range(2,5):
+      tournamentControler.run_round(i)
+tournamentControler.tournament_db()
